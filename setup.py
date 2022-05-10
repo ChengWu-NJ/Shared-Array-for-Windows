b5995e912667fdb29b70ed463efab7a67f8bfcce
ChengWu-NJ <greatdolphin@gmail.com>
2022-05-10 16:03:03 +0800

    add new func update_ndarray_sh

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ ext_modules = [
 
 setup(
         name = 'winsharedarray',
-        version = '1.3',
+        version = '1.3.1',
         include_dirs = [np.get_include()], #Add Include path of numpy
         ext_modules = ext_modules
       )
